aebafb06d228cb1b529f77c25d37e763a59892b0
leonardpak <leonard.eugene.pak@gmail.com>
2024-05-13 21:43:25 +0900

    Removal of Climate Corporation piece
    Too old and too much work to salvage

--- content/climate/deliverables/climatecom_mobile_prototype/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k)])]);}; 
-var b="rootNodes",c="pageName",d="iPhone 4S Frame",e="type",f="Wireframe",g="url",h="iPhone_4S_Frame.html",i="children",j="iPhone 4S Screen",k="iPhone_4S_Screen.html";
-return _creator();
-})();
